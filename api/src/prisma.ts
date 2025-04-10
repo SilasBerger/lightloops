e1c19d6052592e14e9c0f3cc234e16ee3d5a8123
Silas Berger <silas.berger@outlook.com>
2025-04-10 22:45:45 +0200

    Copy some db scripts from teaching-api.

---
+++ api/src/prisma.ts
@@ -0,0 +1,37 @@
+// Source: https://github.com/GBSL-Informatik/teaching-api/blob/main/src/prisma.ts
+
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const options: Prisma.PrismaClientOptions & {
+    log: (Prisma.LogDefinition | { emit: 'event'; level: 'query' })[];
+} = {} as any;
+if (process.env.LOG) {
+    options.log = [
+        {
+            emit: 'event',
+            level: 'query'
+        },
+        {
+            emit: 'stdout',
+            level: 'error'
+        },
+        {
+            emit: 'stdout',
+            level: 'info'
+        },
+        {
+            emit: 'stdout',
+            level: 'warn'
+        }
+    ];
+}
+const prisma = new PrismaClient(options);
+prisma.$connect();
+
+if (process.env.LOG) {
+    prisma.$on('query', (e) => {
+        console.log(`Query: ${e.query}; ${e.params.slice(0, 120)}; -- ${e.duration}ms`);
+    });
+}
+
+export default prisma;
