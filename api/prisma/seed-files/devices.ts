2c2b8337f2a0045e1ffd9f85e308b45c410286a7
Silas Berger <silas.berger@outlook.com>
2025-04-10 22:54:47 +0200

    Create first seed file.

---
+++ api/prisma/seed-files/devices.ts
@@ -0,0 +1,23 @@
+import { Prisma } from '@prisma/client';
+
+export const CLAMPED_LOOP_1_DEVICE_ID = '09756982-154d-43f9-bffa-c820b3a8e1fe';
+export const CLAMPED_LOOP_2_DEVICE_ID = '5d333842-1d01-410c-a311-2ce1fa6e06a3';
+export const ARCA_LOOP_1_DEVICE_ID = '205e8dce-13f7-49ef-9c61-e612b3cc80b6';
+
+const devices: Prisma.DeviceCreateInput[] = [
+    {
+        id: CLAMPED_LOOP_1_DEVICE_ID,
+        name: 'Clamped Loop 1',
+    },
+    {
+        id: CLAMPED_LOOP_2_DEVICE_ID,
+        name: 'Clamped Loop 2',
+    },
+    {
+        id: ARCA_LOOP_1_DEVICE_ID,
+        name: 'Arca Loop 1',
+        description: 'Loop mounted to GorillaPod'
+    }
+];
+
+export { devices };
--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,1 +1,20 @@
+import prisma from '../src/prisma';
+import { devices as devicesInput } from './seed-files/devices';
+
 console.log('Seeding database...');
+
+async function main() {
+    const devices = await prisma.device.createMany({
+        data: devicesInput,
+    })
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
