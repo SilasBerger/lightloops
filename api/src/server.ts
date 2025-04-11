843e35e751e237b9bc6ea25822a529f319d2cea6
Silas Berger <silas.berger@outlook.com>
2025-04-11 10:21:46 +0200

    Add logger from teaching-api.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,11 @@
 import app from './app';
+import Logger from './utils/logger';
 
 // TODO: Create WS server and have both REST and WS listen on the same port; create WS endpoint.
 
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
-    // TODO: Copy logger from teaching-api and use it here.
-    console.log(`Application is running at: http://localhost:${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+    Logger.info(`Application is running at: http://localhost:${PORT}.`);
+    Logger.info('Press Ctrl+C to quit.');
 });
