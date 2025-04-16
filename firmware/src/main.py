4377ed71a271a33841f0fb5408242a12a61d0837
Silas Berger <silas.berger@outlook.com>
2025-04-16 11:26:20 +0200

    Create first LED animation.

--- firmware/src/main.py
+++ firmware/src/main.py
@@ -1,6 +1,26 @@
-import micropython
-import machine
-import foo.demo
+from machine import Pin
+import neopixel
+import time
 
-micropython.mem_info()  # Shows heap memory usage
-print(machine.freq())  # Displays CPU frequency (typically 240000000 Hz)
+LED_COUNT = 30
+DATA_PIN = 4
+
+strip = neopixel.NeoPixel(Pin(DATA_PIN), LED_COUNT, bpp=3)  # bpp=4 for RGBW, 3 for RGB
+
+DELAY = 0.01
+COLOR = (0, 100, 50)
+
+for round in range(5):
+    for i in range(LED_COUNT):
+        strip[i] = COLOR
+        strip.write()
+        time.sleep(DELAY)
+        strip[i] = (0, 0, 0)
+        strip.write()
+    
+    for i in reversed(range(LED_COUNT)):
+        strip[i] = COLOR
+        strip.write()
+        time.sleep(DELAY)
+        strip[i] = (0, 0, 0)
+        strip.write()
