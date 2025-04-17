631d364da174c111e5dd9a48ac21cff289874fd2
Silas Berger <silas.berger@outlook.com>
2025-04-17 08:48:36 +0200

    Create two LED animations.

--- firmware/src/main.py
+++ firmware/src/main.py
@@ -7,20 +7,36 @@ DATA_PIN = 4
 
 strip = neopixel.NeoPixel(Pin(DATA_PIN), LED_COUNT, bpp=3)  # bpp=4 for RGBW, 3 for RGB
 
-DELAY = 0.01
-COLOR = (0, 100, 50)
 
-for round in range(5):
-    for i in range(LED_COUNT):
-        strip[i] = COLOR
-        strip.write()
-        time.sleep(DELAY)
-        strip[i] = (0, 0, 0)
-        strip.write()
-    
-    for i in reversed(range(LED_COUNT)):
-        strip[i] = COLOR
-        strip.write()
-        time.sleep(DELAY)
-        strip[i] = (0, 0, 0)
-        strip.write()
+def bounce():
+    delay = 0.01
+    color = (0, 255, 100)
+
+    for round in range(5):
+        for i in range(LED_COUNT):
+            strip[i] = color
+            strip.write()
+            time.sleep(delay)
+            strip[i] = (0, 0, 0)
+            strip.write()
+        
+        for i in reversed(range(LED_COUNT)):
+            strip[i] = color
+            strip.write()
+            time.sleep(delay)
+            strip[i] = (0, 0, 0)
+            strip.write()
+
+def circle():
+    delay = 0.01
+    color = (0, 255, 100)
+
+    for round in range(20):
+        for i in range(LED_COUNT):
+            strip[i] = color
+            strip.write()
+            time.sleep(delay)
+            strip[i] = (0, 0, 0)
+            strip.write()
+
+circle()
