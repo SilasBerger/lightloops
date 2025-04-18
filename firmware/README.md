11d689176cb0b5374d3845be13ee7bdfb46c17b4
Silas Berger <silas.berger@outlook.com>
2025-04-18 13:59:14 +0200

    Introduce dev CLI command.

--- firmware/README.md
+++ firmware/README.md
@@ -1,21 +1,31 @@
 # LightLoops Firmware
-## Setup
-- `python3 -m venv venv`
-- `source ./venv/bin/activate`
-- `pip install -r requirements.txt`
-- `cp example.config.json src/config.json` and fill in the correct values.
+## Hardware setup
+Flash the MicroPython firmware to the ESP32 device, e.g. using [this repo](https://github.com/SilasBerger/nodemcu-setup?tab=readme-ov-file#flashing-firmware).
 
-**Note:** The `/src` directory contains a `device_id` file to support development using the `mpremote mount` command.
+## Firmware setup
+- Run `python3 -m venv venv` to create a virtualenv.
+- Run `source ./venv/bin/activate` to activate the virtualenv. This is is also required for interacting with any of the available CLIs.
+- Run `pip install -r requirements.txt` to install all required packages.
+- Run `cp example.config.json config.json` and fill in the correct values. This file can also be moved/copied to a different location (e.g. `~/lightloops.config.json`).
 
-## CLI Usage
-The CLI is used to install and configure the LightLoops firmware on ESP32 devices.
+### Additional development setup
+- Run `./lldevice.py write-config -f /path/to/config.json` to ensure a valid config file is present on the device. This is required when using `./lldevice.py dev`.
+
+## Working with the various CLIs
+### `lldevice.py`
+The CLI is used to install and configure the LightLoops firmware on ESP32 devices, as well as during development. As a non-developer, this should be the only CLI you will need to use.
 
 Run `./lldevice --help` to see a list of available commands.
 
-## ESP32 Development
-- Flash the MicroPython firmware to the ESP32 device, e.g. using [this repo](https://github.com/SilasBerger/nodemcu-setup?tab=readme-ov-file#flashing-firmware).
+### `mpremote`
+MicroPython's default CLI for interacting with hardware devices. Can be used for interacting with the device's file system, as well as for running Python code on the device. `mpremote` is also used by the `lldevice.py`. For non-developers, the direct use of `mpremote` should generally not be necessary.
+
+These are some useful `mpremote` commands that can come in handy during development:
 - List files on the device: `./mpremote fs ls`.
 - Copy a file to the device: `./mpremote fs cp src/main.py :main.py` (remote parts start with a `:`).
 - Run a script on the device (no need to copy it over first): `./mpremote run src/main.py`. **Caution:** Imports will be resolved on the device.
-- Mount and execute a folder on the device (no need to copy it over first): `./mpremote mount src/ exec "import main"`.
+- Mount and execute a folder on the device (no need to copy it over first): `./mpremote mount src/ exec "import main"`. **Caution:** Files outside this directory (including files on the device itself) will not be available.
 - Run the main file on the device (after copying it over): `./mpremote`, then `import main`. 
+
+### `esptool`
+Mostly used for flashing firmware to ESP32 devices. Not particularly relevant to this project.
--- firmware/lldevice.py
+++ firmware/lldevice.py
@@ -3,6 +3,9 @@
 import subprocess
 import uuid
 import os
+import sys
+import shutil
+import json
 from pathlib import Path
 from cleo.commands.command import Command
 from cleo.helpers import argument, option
@@ -10,6 +13,16 @@ from cleo.application import Application
 
 
 MPREMOTE = "./venv/bin/mpremote"
+DEVICE_ID_FILENAME = "device_id"  # Name of the device ID file on the device.
+CONFIG_FILENAME = "config.json"  # Name of the config file on the device.
+
+
+def exit_with_error(command = None, error_msg = "Unknown error."):
+    if command:
+        command.line(f"<error>{error_msg}</error>")
+    else:
+        print(error_msg, file=sys.stderr)
+    sys.exit(1)
 
 
 def read_device_id():
@@ -26,23 +39,40 @@ def read_device_id():
         return None
     
 
-def generate_device_id(overwrite = False):
+def ensure_device_id(command = None, overwrite = False):
     existing_device_id = read_device_id()
     if existing_device_id is not None and not overwrite:
         return
 
     device_id = uuid.uuid4()
-    with open("device_id", "w") as f:
+    with open(DEVICE_ID_FILENAME, "w") as f:
         f.write(str(device_id))
     try:
         subprocess.run(
-            [MPREMOTE, "fs", "cp", "device_id", ":device_id"],
+            [MPREMOTE, "fs", "cp", DEVICE_ID_FILENAME, ":device_id"],
             check=False,
             stdout = subprocess.DEVNULL,
             stderr = subprocess.DEVNULL
         )
+        return device_id
+    except Exception as e:
+        exit_with_error(command, str(e))
     finally:
-        os.remove("device_id")
+        os.remove(DEVICE_ID_FILENAME)
+
+
+def read_config():
+    try:
+        result = subprocess.run(
+            [MPREMOTE, "fs", "cat", ":config.json"],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+
+        return result.stdout.strip(), None
+    except Exception as e:
+        return None, str(e)
 
 
 def write_config(config_file):
@@ -74,8 +104,10 @@ class InstallCommand(Command):
             [MPREMOTE, "fs", "cp", "-r"] + src_files + [":"],
             check=True
         )
-        generate_device_id(overwrite=False)
-        write_config(self.argument("config"))
+        ensure_device_id(self, overwrite=False)
+        error = write_config(self.argument("config"))
+        if error:
+            exit_with_error(self, f"Encountered error while trying to write config: {error}. Installation aborted.")
         self.line("<comment>LightLoops firmware installed successfully!</comment>")
 
 
@@ -91,8 +123,8 @@ class UninstallCommand(Command):
     ]
 
     OPTIONAL_PROTECTED_ROOT_FILES = [
-        "device_id",
-        "config.json",
+        DEVICE_ID_FILENAME,
+        CONFIG_FILENAME,
     ]
 
     def handle(self):
@@ -118,7 +150,7 @@ class UninstallCommand(Command):
                 "-r", 
                 f":{entry}"
             ]
-            subprocess.run(delete_cmd, check=False)  # Continue on individual failures
+            subprocess.run(delete_cmd, check=False)
 
         self.line("<comment>Cleanup complete.</comment>")
 
@@ -137,9 +169,8 @@ class RegenerateDeviceIdCommand(Command):
     description = "Regenerate the device ID on the connected device."
 
     def handle(self):
-        generate_device_id(overwrite=True)
-        device_id = read_device_id()
-        self.line("<comment>Device ID regenerated successfully: {}</comment>".format(device_id))
+        device_id = ensure_device_id(self, overwrite=True)
+        self.line(f"<comment>Device ID regenerated successfully: {device_id}</comment>")
 
 
 class ReadConfigCommand(Command):
@@ -147,17 +178,10 @@ class ReadConfigCommand(Command):
     description = "Read the configuration from the connected device."
 
     def handle(self):
-            try:
-                result = subprocess.run(
-                    [MPREMOTE, "fs", "cat", ":config.json"],
-                    check=True,
-                    capture_output=True,
-                    text=True,
-                )
-
-                self.line(result.stdout.strip())
-            except Exception as e:
-                self.line("<error>No configuration file found.</error>")
+        config, error = read_config()
+        if error:
+            exit_with_error(self, f"Error reading config: {error}.")
+        self.line(config)
 
 
 class WriteConfigCommand(Command):
@@ -171,11 +195,43 @@ class WriteConfigCommand(Command):
         config_file = self.argument("config")
         error = write_config(config_file)
         if error:
-            self.line("<error>Failed to write configuration file: {}</error>".format(error))
+            exit_with_error(f"Failed to write configuration file: {error}.")
         else:
             self.line("<comment>Configuration file written successfully.</comment>")
 
 
+class DevCommand(Command):
+    name = "dev"
+    description = "Run the contents of the `src` directory on the device, using `main.py` as the entry point and using the `device_id` and `config.json` files from the device."
+
+    def handle(self):
+        src_path = Path('src')
+        build_path = Path('build')
+
+        ensure_device_id(self)
+
+        if build_path.exists():
+            shutil.rmtree(build_path)
+        shutil.copytree(src_path, build_path)
+
+        config, error = read_config()
+        if error:
+            exit_with_error(self, "Error reading config on device. Write config to the device to fix this issue.")
+        with open(build_path / 'config.json', "w") as outfile:
+            json.dump(json.loads(config), outfile, indent=2)
+
+        device_id = read_device_id()
+        if not device_id:
+            exit_with_error(self, "Unexpected error: Unable to read device ID.")
+        with open(build_path / DEVICE_ID_FILENAME, "w") as outfile:
+            outfile.write(device_id)
+
+        subprocess.run(
+            [MPREMOTE, "mount", "build/", "exec", "import main"],
+            check=True
+        )
+
+        
 application = Application()
 application.add(InstallCommand())
 application.add(UninstallCommand())
@@ -183,6 +239,8 @@ application.add(ReadDeviceIdCommand())
 application.add(RegenerateDeviceIdCommand())
 application.add(ReadConfigCommand())
 application.add(WriteConfigCommand())
+application.add(DevCommand())
+
 
 if __name__ == "__main__":
     application.run()
